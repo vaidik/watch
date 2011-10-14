01188d3db51555b4258cf8aa7f3077efa12240ee
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2011-10-14 15:12:19 +0530

    Minor bug fix in get_arg(). Script is complete and functional. Script is also executable.

--- watch.py
+++ watch.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python
+
 # Watch - program to execute commands after a set interval
 # Author: Vaidik Kapoor  <kapoor.vaidik@gmail.com>
 
@@ -14,27 +16,69 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
+import sys, shlex, subprocess, time
 
 '''
 Gets command-line arguments
 '''
 def get_arg(arg, size=1):
-	argc = len(sys.argv)
+  argc = len(sys.argv)
 
-	i = 0
-	for i in range(0, argc):
-		if sys.argv[i] == arg:
-			try:
-				arg_val = []
-				j = 0
-				for j in range(1, size+1):
-					arg_val.append(sys.argv[i+j])
+  i = 0
+  for i in range(0, argc):
+    if sys.argv[i] == arg:
+      if size == 0:
+        return True
 
-				if len(arg_val) > 0:
-					return arg_val
-			except IndexError:
-				print "%s option needs a value as an argument." % arg
+      try:
+        arg_val = []
+        j = 0
+        for j in range(1, size+1):
+          arg_val.append(sys.argv[i+j])
 
-	return None
+        if len(arg_val) > 0:
+          return arg_val
+      except IndexError:
+        print "Error: %s option needs %s value(s) as argument(s)." % (arg, size)
+        exit()
+
+  return None
+
+if __name__ == '__main__':
+  argl = 1
+
+  interval = get_arg('--interval')
+  try:
+    if interval != None:
+      interval = int(interval[0])
+      argl = argl + 2
+    else:
+      '''
+      Default value of interval is 2 seconds.
+      '''
+      interval = 2
+  except ValueError:
+    print "--interval option expects time (integer) in seconds"
+    exit()
+
+  shell = get_arg('--shell', 0)
+  if shell == True:
+    print "Enter command:"
+    command = raw_input()
+    argl = argl + 1
+  else:
+    try:
+      command = sys.argv[argl]
+    except IndexError:
+      print "Error: You did not provide a command to execute."
+      exit()
+
+  while True:
+    try:
+      args = shlex.split(command)
+      process = subprocess.Popen(args)
+      time.sleep(interval)
+    except OSError:
+      print "Error: The command could not be executed. Check the command you supplied."
+      exit()
 
